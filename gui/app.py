0a225b8ec12eae40503dc3981e121c790360ec62
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-09-30 21:02:31 +0200

    Aplicacion iniciada

--- gui/app.py
+++ gui/app.py
@@ -1,10 +1,27 @@
+from os import path as op
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+path = op.dirname(op.realpath(__file__))
+print(f'Ruta = {path}')
+path_kv = op.join(path, 'kv')
+print(f'KV = {path_kv}')
+path_utils = op.join(path_kv, 'utils.kv')
+print(f'Utils = {path_utils}')
+path_find_device = op.join(path_kv, 'find_device.kv')
+
+Builder.load_file(op.join(path_kv, 'screens.kv'))
+
+
+class BluiGui(BoxLayout):
+    pass
 
 
 class MyApp(App):
     def build(self):
-        return Button(text='Hola mundo')
+        return BluiGui()
 
 if __name__ == '__main__':
     MyApp().run()
